0176c56fd31081eca8420a9a5aad3a0937643a1d
Wissamkadi <164694142+Wissamkadi@users.noreply.github.com>
2025-08-31 19:56:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -209,6 +209,11 @@ app.post("/signin", async (req,res) => {
      }
    })
 
+app.get("/", (req, res) => {
+  res.json({ message: "Server is up and running!" });
+});
+
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
